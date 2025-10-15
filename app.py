760b468235a8a7079da0f68865c7a83343641b9d
wasiftak <wtak732@gmail.com>
2025-10-15 14:03:18 +0530

    Enhance

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, Response, jsonify
 import cv2
 import config
 from game_manager import GameManager
+
 app = Flask(__name__)
 face_cascade = cv2.CascadeClassifier(config.FACE_CASCADE_PATH)
 video_capture = cv2.VideoCapture(0)
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, config.SCREEN_WIDTH)
 video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, config.SCREEN_HEIGHT)
 game_manager = GameManager()
+
 def generate_frames():
     global game_manager
     while True:
@@ -29,14 +31,17 @@ def generate_frames():
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 @app.route("/game")
 def game():
     global game_manager
     game_manager.reset_game()
     return render_template("game.html")
+
 @app.route("/video_feed")
 def video_feed():
     return Response(generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
@@ -48,7 +53,8 @@ def status():
 @app.route("/gameover")
 def gameover():
     score = game_manager.score
-    return render_template("gameover.html", score=score)
+    high_score = game_manager.high_score
+    return render_template("gameover.html", score=score, high_score=high_score)
 
 if __name__ == "__main__":
     app.run(debug=True)
